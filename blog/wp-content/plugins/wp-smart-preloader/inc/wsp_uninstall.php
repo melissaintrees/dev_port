b4743b7a50f345c692ea85eb9abc1be75e298f8c
“melissaintrees” <melissaintrees@gmail.com>
2024-05-26 17:07:22 -0500

    ripping out the wp blog for portfolio updates

--- blog/wp-content/plugins/wp-smart-preloader/inc/wsp_uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * uninstall hook
- */
- register_uninstall_hook( __FILE__, array( 'WP_smart_preloader', 'wsp_uninstall' ) );
--- blog/wp-content/themes/creative-company/section-parts/section-map.php
+++
@@ -1,5 +0,0 @@
-<!-- Map -->
-<div id="gmap" >	<!-- <div id="map"></div> -->
-	<?php dynamic_sidebar( 'map' ); ?>
-</div>
-<!-- End Map -->	
